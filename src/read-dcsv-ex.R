c499c0495190e30bca96f027080ca10a7d2fa766
Jason Cairns <jason.cairns@auckland.ac.nz>
2023-09-28 20:26:21 +1300

    fix spacing after package macros

--- src/read-dcsv-ex.R
+++ src/read-dcsv-ex.R
@@ -1,1 +1,3 @@
-ddf <- read.dcsv(c("host1:/filepath/to/file1.csv", "host2:/filepath/to/file2.csv", "host3:/filepath/to/file3.csv"))
+ddf <- read.dcsv(c("host1:/filepath/to/file1.csv",
+		   "host2:/filepath/to/file2.csv",
+		   "host3:/filepath/to/file3.csv"))
