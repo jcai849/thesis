f5889e5290d6cbd91d06ac81c4317a19dbf027a0
Jason Cairns <jason.cairns@auckland.ac.nz>
2023-10-09 18:08:26 +1300

    iris->taxicab, flesh out lasso chapter slightly

--- src/lm-reduce.R
+++ src/lm-reduce.R
@@ -1,4 +1,4 @@
 nc <- 15
-cs <- split(iris, cumsum((seq(nrow(iris)) - 1) %% nc == 0)) # split to chunks
+cs <- split(iris, cumsum((seq(nrow(taxicab)) - 1) %% nc == 0)) # split to chunks
 model <- Reduce(f = update, x = cs[-1],
-		init = biglm(Petal.Length ~ Sepal.Width * Sepal.Length, cs[[1]]))
+		init = biglm(Tip_amount ~ Passenger_count * Fare_amount, cs[[1]]))
