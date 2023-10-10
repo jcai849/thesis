55d58aa47c2861daf1b6167ecbd07cc2f254d24a
Jason Cairns <jason.cairns@auckland.ac.nz>
2023-10-10 19:40:16 +1300

    fix refs and figs

--- src/lm-reduce.R
+++ src/lm-reduce.R
@@ -1,4 +1,4 @@
 nc <- 15
-cs <- split(iris, cumsum((seq(nrow(taxicab)) - 1) %% nc == 0)) # split to chunks
+cs <- split(taxicab, cumsum((seq(nrow(taxicab)) - 1) %% nc == 0)) # split to chunks
 model <- Reduce(f = update, x = cs[-1],
 		init = biglm(Tip_amount ~ Passenger_count * Fare_amount, cs[[1]]))
